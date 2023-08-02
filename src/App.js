80e87aa75be637c28191f4b152d1fe344ae84f26
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-08-02 12:12:39 +0000

    project completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Switch, Route} from 'react-router-dom'
 
 import LoginForm from './components/LoginForm'
+import Home from './components/Home'
 import NotFound from './components/NotFound'
 
 import './App.css'
@@ -8,6 +9,7 @@ import './App.css'
 const App = () => (
   <Switch>
     <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/" component={Home} />
     <Route component={NotFound} />
   </Switch>
 )
